4ff585921ff52f24061f33f0bcf239fb3c62e724
Sangeet Banik <142200832+sangeetbanik@users.noreply.github.com>
2024-10-28 11:23:53 +0530

    Data_combination.py

--- Data_combination.py
+++ Data_combination.py
@@ -10,5 +10,11 @@ from scipy import stats
 --combining all the 12 csv iles into a single csv file.
 csv_files = [r"file_path\01-2023.csv" ,r"file_path\02-2023.csv",r"file_path\03-2023.csv",r"file_path\04-2023.csv",r"file_path\05-2023.csv",r"file_path\06-2023.csv",r"file_path\07-2023.csv",r"file_path\08-2023.csv",r"ṅ\09-2023.csv",r"file_path\10-2023.csv",r"file_path\11-2023.csv",r"file_path\12-2023.csv"]
 cyclistic_data = pd.concat([pd.read_csv(file) for file in csv_files])
-combined_csv.reset_index(drop=True, inplace=True)
+cyclistic_data.reset_index(drop=True, inplace=True)
+
+--saving the new table.
+cyclistic_data_path = r"C:\Users\Pratik Banik\Downloads\GDA_Project_1\2023-08 to 2024-08\clean files.csv"
+cyclistic_data.to_csv('cyclistic_data_path' ,index=False)
+print("CSV files have been successfully combined!")
+
 
